0bbd06cc2c9e33dc03c513f19afe9c4a8bed5753
Matt Wynne <matt@mattwynne.net>
2012-01-06 18:08:43 +0000

    Remove cruft

--- spec/apple_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'support/fruit_examples'
-require 'apple'
-
-describe Apple do
-  it_should_behave_like "a fruit"
-
-  it { should have_pips }
-end
--- spec/support/fruit_examples.rb
+++
@@ -1,3 +0,0 @@
-shared_examples_for 'a fruit' do
-  it { should be_sweet }
-end
