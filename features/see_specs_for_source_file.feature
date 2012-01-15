38f1dc31793a23183190947037b5a2d77ae7d078
Matt Wynne <matt@mattwynne.net>
2012-01-15 22:12:47 +0000

    Trap a bug

--- features/see_specs_for_source_file.feature
+++ features/see_specs_for_source_file.feature
@@ -5,7 +5,7 @@ Feature: See specs for source file
     Given two classes, each with corresponding RSpec specs:
       | interesting.rb |
       | stable.rb      |
-    And Darts has watched the specs run once
+    And Darts has watched the specs run twice
     When I ask Darts which specs touch "lib/interesting.rb"
     Then I should be given this list:
       | spec/interesting_spec.rb:5 |
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -4,8 +4,8 @@ Given /^two classes, each with corresponding RSpec specs:$/ do |table|
   end
 end
 
-Given /^Darts has watched the specs run once$/ do
-  run_specs_with_darts
+Given /^Darts has watched the specs run twice$/ do
+  2.times { run_specs_with_darts }
 end
 
 When /^I ask Darts which specs touch "([^"]*)"$/ do |source_file|
@@ -25,7 +25,7 @@ When /^I ask Darts which specs I should run$/ do
 end
 
 Then /^I should be given this list:$/ do |table|
-  table.raw.flatten.should == output_from_darts.split("\n")
+  output_from_darts.split("\n").should == table.raw.flatten
 end
 
 Then /^I should see:$/ do |string|
